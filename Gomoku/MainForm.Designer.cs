03650248aad886d231e85b61e375e31e151df736
peter28 <42987392+Gpeter28@users.noreply.github.com>
2021-06-13 15:46:43 +0800

    Upload
    Init

---
+++ Gomoku/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Gomoku
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Restart = new System.Windows.Forms.Button();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.PicBox_Board = new System.Windows.Forms.PictureBox();
+            this.lbl_ColorOfChess = new System.Windows.Forms.Label();
+            this.lbl_Turns = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Restart
+            // 
+            this.Btn_Restart.Location = new System.Drawing.Point(890, 12);
+            this.Btn_Restart.Name = "Btn_Restart";
+            this.Btn_Restart.Size = new System.Drawing.Size(106, 74);
+            this.Btn_Restart.TabIndex = 0;
+            this.Btn_Restart.Text = "Restart";
+            this.Btn_Restart.UseVisualStyleBackColor = true;
+            this.Btn_Restart.Click += new System.EventHandler(this.Btn_Restart_Click);
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Enabled = false;
+            this.Btn_Start.Location = new System.Drawing.Point(890, 92);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(106, 74);
+            this.Btn_Start.TabIndex = 1;
+            this.Btn_Start.Text = "Start";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            // 
+            // PicBox_Board
+            // 
+            this.PicBox_Board.BackColor = System.Drawing.Color.Transparent;
+            this.PicBox_Board.Image = global::Gomoku.Properties.Resources.borad;
+            this.PicBox_Board.Location = new System.Drawing.Point(10, 10);
+            this.PicBox_Board.Margin = new System.Windows.Forms.Padding(0);
+            this.PicBox_Board.Name = "PicBox_Board";
+            this.PicBox_Board.Size = new System.Drawing.Size(810, 802);
+            this.PicBox_Board.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PicBox_Board.TabIndex = 2;
+            this.PicBox_Board.TabStop = false;
+            this.PicBox_Board.Click += new System.EventHandler(this.PicBox_Board_Click);
+            // 
+            // lbl_ColorOfChess
+            // 
+            this.lbl_ColorOfChess.AutoSize = true;
+            this.lbl_ColorOfChess.Font = new System.Drawing.Font("宋体", 25F);
+            this.lbl_ColorOfChess.Location = new System.Drawing.Point(884, 385);
+            this.lbl_ColorOfChess.Name = "lbl_ColorOfChess";
+            this.lbl_ColorOfChess.Size = new System.Drawing.Size(83, 34);
+            this.lbl_ColorOfChess.TabIndex = 19;
+            this.lbl_ColorOfChess.Text = "颜色";
+            // 
+            // lbl_Turns
+            // 
+            this.lbl_Turns.AutoSize = true;
+            this.lbl_Turns.Font = new System.Drawing.Font("宋体", 25F);
+            this.lbl_Turns.Location = new System.Drawing.Point(884, 304);
+            this.lbl_Turns.Name = "lbl_Turns";
+            this.lbl_Turns.Size = new System.Drawing.Size(117, 34);
+            this.lbl_Turns.TabIndex = 20;
+            this.lbl_Turns.Text = "第几步";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 818);
+            this.Controls.Add(this.lbl_Turns);
+            this.Controls.Add(this.lbl_ColorOfChess);
+            this.Controls.Add(this.Btn_Start);
+            this.Controls.Add(this.Btn_Restart);
+            this.Controls.Add(this.PicBox_Board);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Board)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Restart;
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.PictureBox PicBox_Board;
+        private System.Windows.Forms.Label lbl_ColorOfChess;
+        private System.Windows.Forms.Label lbl_Turns;
+    }
+}
+
---
+++ Gomoku/MainForm.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gomoku.Properties;
+
+namespace Gomoku
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private int turns = 0;
+
+        // 1 for black     -1 for white
+        private int colors = 1;
+
+        private List<Chess> whiteChesses = new List<Chess>();
+        private List<Chess> blackChesses = new List<Chess>();
+
+        private int[,] chessArray = new int[15, 15];
+
+        private readonly int offsetX = 3;
+        private readonly int offsetY = 3;
+        private void PicBox_Board_Click(object sender, EventArgs e)
+        {
+            Point mousePostion = MousePosition;
+
+            mousePostion.X -= 25;
+            mousePostion.Y -= 25;
+
+            // CheckIfOutBorad
+
+            // CheckBoard(mousePostion);
+
+            var pointOfBoard = PicBox_Board.PointToClient(mousePostion);
+
+            // CheckIf the Position Is Occupied
+
+            // CheckOccupy(pointOfBoard);
+
+
+
+            var color = colors == 1 ? ChessColor.BLACK : ChessColor.WHITE;
+            var offsetPoint = GetOffsetPoint(pointOfBoard);
+            Chess newChess = new Chess()
+            {
+                ChessColor = color,
+                Location = offsetPoint,
+                Image = color == ChessColor.BLACK ? Resources.black : Resources.white,
+                Name = "Chess" + "_Colors_" + color.ToString() + "_turns_" + turns, 
+                Size = new Size(50, 50)
+            };
+
+            PicBox_Board.Controls.Add(newChess);
+            if (newChess.ChessColor == ChessColor.BLACK)
+            {
+                chessArray[offsetPoint.X / 54, offsetPoint.Y / 54] = 1;
+                Console.WriteLine($"{offsetPoint.X / 54}, {offsetPoint.Y / 54}, black");
+            }
+            else
+            {
+                chessArray[offsetPoint.X / 54, offsetPoint.Y / 54] = -1;
+                Console.WriteLine($"{offsetPoint.X / 54}, {offsetPoint.Y / 54}, white");
+            }
+
+            UpdateLabel();
+            var flag = CheckWin(color, offsetPoint.X / 54, offsetPoint.Y / 54);
+            if (flag)
+            {
+                MessageBox.Show($"Win");
+            }
+
+
+            turns++;
+            colors = -colors;
+        }
+
+
+        private int chessOffset = 54;
+        private Point GetOffsetPoint(Point p)
+        {
+            var x = p.X - offsetX;
+            var y = p.Y - offsetY;
+
+
+            var modX = x % chessOffset;
+            var modY = y % chessOffset;
+
+            var divideX = x / chessOffset;
+            var divideY = y / chessOffset;
+
+            // 27 for half of chessOffset;
+
+            if (modX > chessOffset / 2) divideX++;
+
+            if (modY > chessOffset / 2) divideY++;
+
+            return new Point(divideX * chessOffset + offsetX, divideY * chessOffset + offsetY);
+        }
+
+        private void CheckBoard(Point point)
+        {
+            // 15, 750 for Borad Size
+
+            if (point.X < 15 || point.Y < 15) return;
+
+            if (point.X > 750 || point.Y > 750) return;
+        }
+
+        private void CheckOccupy(Point p)
+        {
+            var x = p.X - offsetX;
+            var y = p.Y - offsetY;
+
+
+            var modX = x % chessOffset;
+            var modY = y % chessOffset;
+
+            var divideX = x / chessOffset;
+            var divideY = y / chessOffset;
+
+            if (modX > 27) divideX++;
+
+            if (modY > 27) divideY++;
+        }
+
+        private bool CheckWin(ChessColor color, int positionX, int positionY)
+        {
+            if(turns < 5) return false;
+            var flag = false;
+
+            for (int i = -1; i <= 1; ++i)
+            {
+                for (int j = -1; j <= 1; ++j)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    var nowX = positionX + i;
+                    var nowY = positionY + j;
+                    if (nowX < 0 || nowX > 14 || nowY < 0 || nowY > 14) continue;
+
+                    if (chessArray[nowX, nowY] == (color == ChessColor.BLACK ? 1 : -1))
+                    {
+                        flag = CheckLine(nowX, nowY, i, j, color);
+                        if (flag)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return flag;
+        }
+
+
+        private bool CheckLine(int x, int y, int i, int j, ChessColor color)
+        {
+            int remainX = i * 3;
+            int remainY = j * 3;
+
+            if (x + remainX < 0 || x + remainX > 14 || y + remainY < 0 || y + remainY > 14) return false;
+
+
+            int times = 0;
+
+            for (int t = 0; t < 3; ++t)
+            {
+                x += i;
+                y += j;
+                if (chessArray[x, y] == (color == ChessColor.BLACK ? 1 : -1))
+                {
+                    ++times;
+                }
+            }
+
+            Console.WriteLine(times);
+            return times == 3 ? true : false;
+        }
+
+        private void UpdateLabel()
+        {
+            this.lbl_ColorOfChess.Text = colors == 1 ? "黑色" : "白色";
+            this.lbl_ColorOfChess.ForeColor = colors == 1 ? Color.Black : Color.White;
+
+
+            this.lbl_Turns.Text = turns.ToString();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Restart_Click(object sender, EventArgs e)
+        {
+            turns = 0;
+            colors = 1;
+            PicBox_Board.Controls.Clear();
+            chessArray = new int[15, 15];
+        }
+    }
+}
